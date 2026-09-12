3950e6410c57847abc028af0cd37b99a197363bc
Gnlow <gnlow@pm.me>
2026-09-12 19:02:39 +0900

    u

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { Plane } from "https://gnlow.dev/plane@0.1.3"
+import { Plane } from "https://gnlow.dev/plane@0.1.4"
 import { Dist } from "https://raw.esm.sh/gh/gnlow/disty@0.5.0/mod.ts"
 import { arr, mod } from "https://gnlow.dev/util@0.1.2"
 
@@ -8,20 +8,25 @@ export const grow =
     arr(n).forEach(i => {
         const x0 = Dist.range(0, plane.w).pick(""+i)
         const y0 = Dist.range(0, plane.h).pick(""+i)
-        const x1 = Dist.n(x0, plane.w/2).map(x => mod(Math.floor(x), plane.w)).pick(""+i)
-        const y1 = Dist.n(x0, plane.w/2).map(x => mod(Math.floor(x), plane.w)).pick(""+i)
-        const cnt = Dist.poisson(plane.get([x0, y0])!/10).pick(""+i)*10
+        const x1 = Dist.n(x0, 2).map(x => mod(Math.floor(x), plane.w)).pick(""+i)
+        const y1 = Dist.n(y0, 2).map(y => mod(Math.floor(y), plane.h)).pick(""+i)
+        const cnt = plane.get([x0, y0])!
+            * Dist.ll(0.9, 1).pick(""+i)
         
-        plane.add([x1, y1], cnt+1)
+        plane.add([x1, y1], Math.floor(cnt*0.5))
     })
 }
 
 const plane = new Plane<number>(10, 10)
 
 arr(10).forEach(x => arr(10).forEach(y =>
-    plane.add([x, y], 1)
+    plane.set([x, y], 100)
 ))
 
-grow(700)(plane)
+grow(1000)(plane)
 
 console.log(plane.raw.values().toArray().toSorted((a, b)=>b-a))
+await Deno.writeFile("mod.png",
+    plane.map(n => Math.log(n || 1)/5*255)
+    .grayscale().upscale(10).toPng()
+)
